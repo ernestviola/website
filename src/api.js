d0b3e874b9627b9398645668e629e0ce9e7f5a84
Austin Gil <austin@stegosource.com>
2019-02-18 12:10:30 -0800

    Basic API ssetup

---
+++ src/api.js
@@ -0,0 +1,29 @@
+import fetchJsonp from "fetch-jsonp";
+
+// GitHub API Docs: https://developer.github.com/v3/
+const githubHost = "https://api.github.com";
+const githubToken = process.env.VUE_APP_GITHUB; // https://github.com/settings/tokens
+const githubConfig = {
+  headers: new Headers({
+    Authorization: `token ${githubToken}`
+  })
+};
+
+// Meetup API Docs: https://www.meetup.com/meetup_api/
+const meetupHost = "http://api.meetup.com";
+
+export default {
+  githubProjects(username) {
+    return fetch(`${githubHost}/users/${username}/repos`, githubConfig).then(res => res.json());
+  },
+
+  githubProjectDetails({ username, project }) {
+    return fetch(`${githubHost}/repos/${username}/${project}`, githubConfig).then(res =>
+      res.json()
+    );
+  },
+
+  meetupEvents() {
+    return fetchJsonp(`${meetupHost}/sandiegojs/events`).then(res => res.json());
+  }
+};
