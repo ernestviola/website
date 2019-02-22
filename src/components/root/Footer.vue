c566746e4ae86a0919ee4672d0b046c761197502
wishinghand <wishinghand@gmail.com>
2019-02-21 21:01:15 -0800

    Adding footer

---
+++ src/components/root/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="bg-light py-5">
+      <div class="container">
+        <div class="small text-center text-muted">Copyright &copy; {{ currentYear }} - Start Bootstrap</div>
+      </div>
+    </footer>
+</template>
+
+<script>
+const date = new Date()
+export default {
+  data: () => ({
+    currentYear: date.getFullYear()
+  })
+}
+</script>
