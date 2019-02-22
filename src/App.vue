daf11a501d2ac2210d7df384d9a24b56806bc2f3
wishinghand <wishinghand@gmail.com>
2019-02-21 19:51:54 -0800

    Header component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,7 @@
 <template>
   <div id="app">
-    <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto my-2 my-lg-0">
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#services">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
+    <vsd-header />
+    <vsd-header />
   <!-- Masthead -->
   <header class="masthead">
     <div class="container h-100">
@@ -225,6 +201,16 @@
     <router-view /> -->
   </div>
 </template>
+
+<script>
+import VsdHeader from '@/components/root/Header'
+
+export default {
+  components: {
+    VsdHeader
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
